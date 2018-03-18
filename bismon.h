6ef80b68b7bc4e59fc8a49c1ef5364d206d22654
Basile Starynkevitch <basile@starynkevitch.net>
2018-03-18 10:33:30 +0100

    add my email

--- bismon.h
+++ bismon.h
@@ -17,6 +17,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+----
+    Contact me (Basile Starynkevitch) by email
+    basile@starynkevitch.net and/or basile.starynkevitch@cea.fr
 **/
 
 #ifndef _GNU_SOURCE
--- main_BM.c
+++ main_BM.c
@@ -15,6 +15,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+----
+    Contact me (Basile Starynkevitch) by email
+    basile@starynkevitch.net and/or basile.starynkevitch@cea.fr
 ***/
 #include "bismon.h"
 #include <gtk/gtk.h>
